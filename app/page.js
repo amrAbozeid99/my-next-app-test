0cb1215d2bc5f53bf4555531a5d389e688dda1f4
amr apozeid <x.genral990@gmail.com>
2025-02-11 05:48:09 +0200

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div >
+      <h1 className={styles.title}>Hello World</h1>
+      <p className={styles.description}>this im my  .... tray to uplouding my next app</p>
+    </div>
+  );
+}
